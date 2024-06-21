7a77c24a2acd04b1ce7cd8b00fb94ccedd904aa0
SilasZy <tazerfacebr@gmail.com>
2024-06-21 17:59:07 -0300

    treino forEach

---
+++ forEach.js
@@ -0,0 +1,18 @@
+let pessoas = [
+
+{
+nome: 'maria',
+idade: '20'
+
+},
+{
+    nome:'pedro',
+    idade:'15'
+},
+
+]
+pessoas.forEach(function (lista)  {
+console.log(`${'essa pessoa se chama'} ${lista.nome} ${'ela tem'} ${lista.idade} ${'anos'}`)
+
+})
+    
